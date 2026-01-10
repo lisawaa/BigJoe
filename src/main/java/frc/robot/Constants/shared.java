f213d3dbe9263720bbc04f0c106614d8f1772976
Tigears4470 <59905657+Tigears4470@users.noreply.github.com>
2026-01-10 13:47:26 -0800

    Updated constants

--- src/main/java/frc/robot/Constants/shared.java
+++ src/main/java/frc/robot/Constants/shared.java
@@ -1,6 +1,8 @@
 package frc.robot.Constants;
 
 // Shared constants between multiple subsystems
-public class shared {
-    
+public final class Shared {
+    public final static class OperatorConstants {
+          public static final int DRIVER_CONTROLLER_PART = 0;   
+    }
 }
