5780246ac694e7c8116f3ecb30f37cc794a8523e
lisawaa <ywcai2008@gmail.com>
2026-01-16 18:28:46 -0800

    Co-authored-by: Mr. Melon <265185@pylusd.org> Co-authored-by: James Koo <jameskoo3279@gmail.com> Co-authored-by: Vidur Manikandan <280966@pylusd.org>

--- src/main/java/frc/robot/subsystems/KitbotShootingSubsystem.java
+++ src/main/java/frc/robot/subsystems/KitbotShootingSubsystem.java
@@ -10,11 +10,10 @@ import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class KitbotShootingSubsystem extends SubsystemBase{
+public class KitbotShootingSubsystem extends SubsystemBase {
     private SparkMax shootingMotor;
 
-    public KitbotShootingSubsystem()
-    {
+    public KitbotShootingSubsystem() {
         shootingMotor = new SparkMax(ShootingConstants.MotorID, MotorType.kBrushless);
         SparkMaxConfig feederConfig = new SparkMaxConfig();
         feederConfig.smartCurrentLimit(ShootingConstants.SHOOTING_MOTOR_CURRENT_LIMIT);
@@ -22,13 +21,11 @@ public class KitbotShootingSubsystem extends SubsystemBase{
         SmartDashboard.putNumber("Shooting voltage value", ShootingConstants.SHOOTING_VOLTAGE);
     }
 
-    public void shooting()
-    {
+    public void shooting() {
         shootingMotor.setVoltage(ShootingConstants.voltage);
     }
 
-    public void stopMotors()
-    {
+    public void stopMotors() {
         shootingMotor.setVoltage(0.0);
     }
 
